e26646bc05f99e4c615243672d834b9e86d10040
Prashnik <prashnikdas.tal@gmail.com>
2024-04-19 14:14:44 +0530

    "new layout"

--- js/main.js
+++ js/main.js
@@ -34,6 +34,30 @@
         }
     });
 
+    
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            992:{
+                items:2
+            }
+        }
+    });
+
+
 
   
 
